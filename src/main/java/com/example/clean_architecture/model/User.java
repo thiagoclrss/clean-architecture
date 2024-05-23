41eae3394743f95768ff2ba837d768b6e91ec2ea
thiagoclrss <thiagocolaress77@gmail.com>
2024-05-23 11:55:36 -0300

    first commit

---
+++ src/main/java/com/example/clean_architecture/model/User.java
@@ -0,0 +1,49 @@
+package com.example.clean_architecture.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table (name = "tb_users")
+@Entity(name = "tb_users")
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User() {}
+}
---
+++ src/main/java/com/example/clean_architecture/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.clean_architecture.service;
+
+import com.example.clean_architecture.model.User;
+import com.example.clean_architecture.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+}
