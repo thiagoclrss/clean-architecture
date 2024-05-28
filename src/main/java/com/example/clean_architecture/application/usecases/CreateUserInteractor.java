9a01a2a25e7a13c3839960e87f375b6f6f0442a1
thiagoclrss <thiagocolaress77@gmail.com>
2024-05-27 23:59:10 -0300

    update

---
+++ src/main/java/com/example/clean_architecture/application/usecases/CreateUserInteractor.java
@@ -0,0 +1,8 @@
+package com.example.clean_architecture.application.usecases;
+
+import com.example.clean_architecture.domain.entity.User;
+
+public class CreateUserInteractor {
+    public User createUser(User user) {return null;}
+
+}
---
+++ src/main/java/com/example/clean_architecture/domain/entity/User.java
@@ -0,0 +1,4 @@
+package com.example.clean_architecture.domain.entity;
+
+public record User (String username, String password, String email){
+}
